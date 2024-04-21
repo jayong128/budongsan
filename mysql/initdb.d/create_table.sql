72f554ebf24c7c1cfdcf2d705c0bb2c9a6e0c48d
jayong128 <novo200297@gmail.com>
2024-04-21 15:53:54 +0900

    mysql conf

---
+++ mysql/initdb.d/create_table.sql
@@ -0,0 +1,9 @@
+create table cortar
+(
+    cortar_number varchar(10) not null,
+    cortar_name varchar(20) not null,
+    map_x varchar(20),
+    map_y varchar(20),
+    cortar_type varchar(20) not null,
+    primary key (cortar_number)
+)
